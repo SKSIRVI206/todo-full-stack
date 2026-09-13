8290b4040fc8390b816376ef8c0a5d2802ead9d1
SKSIRVI206 <sksirvi206@gmail.com>
2026-09-13 22:31:31 +0530

    correct db connection file

--- backend/dataBaseConnection.js
+++ backend/dataBaseConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDb = async()=>{
     try {
-        const dbConnection = await mongoose.connect(process.env.Db_URL);
+        const dbConnection = await mongoose.connect(process.env.DB_URL);
         console.log('Connected To DB')
     } catch (error) {
         console.log('Not Connected To DB', error);
